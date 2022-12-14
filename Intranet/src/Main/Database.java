4978ea1564e9f2b5fe94ba8e809ad6fb715d474f
Khan Martin <rblxxmartin@gmail.com>
2022-12-14 16:32:33 +0600

    DB upd

--- Intranet/src/Main/Database.java
+++ Intranet/src/Main/Database.java
@@ -1,29 +1,20 @@
 package Main;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public final class Database {
 
-public class Database {
-	private String basePath;
-	private String path;
-	private List<User> users;
-	private List<Student> students;
-	private List<Teacher> teachers;
-	private List<Admin> admins;
-	private List<Manager> managers;
-	private Map<String, Course> courses;
-	private Map<String, List<LogFile>> logFiles;
+	private final static String BASE_PATH = "memememamomo";
+	private static Database instance = new Database(BASE_PATH);
+	private final String path;
 
-	public Database() {
-		this.users = new ArrayList<>();
-		this.students = new ArrayList<>();
-		this.teachers = new ArrayList<>();
-		this.admins = new ArrayList<>();
-		this.managers = new ArrayList<>();
-		this.courses = new HashMap<>();
-		this.logFiles = new HashMap<>();
+	private Database(String path) {
+		this.path = path;
 	}
 
+	public String getPath() {
+		return this.path;
+	}
+
+	public static Database getInstance() {
+		return instance;
+	}
 }
