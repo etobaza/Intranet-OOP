057e6aa56fe69dda3742a671a0d0f2f4c5052709
kalma <kalma@LAPTOP-NDHO64MP>
2022-12-15 01:50:30 +0600

    aoaoao

--- Intranet/src/Main/Employee.java
+++ Intranet/src/Main/Employee.java
@@ -1,13 +1,105 @@
 package Main;
 
-public class Employee extends User {
+import java.util.*;
 
-	private double salary;
+public class Employee extends User{
 
-	public void sendMessage(int Message) {
+	 private double salary;
+
+	    private Vector<Message> messages;
+
+	    {
+
+	     messages =  new Vector<Message>();
+
+	    }
+
+	 public Employee(String firstName, String lastName, String id, String username, String password, Sex sex, int age,
+
+	   String email, double salary){
+
+	  super(firstName, lastName, id, username, password, sex, age, email);
+
+	  this.salary = salary;
+
+	 }
+
+	 
+
+	 
+
+	    public void sendMessage(Employee employee,  Message message){
+
+	     employee.messages.add(message);
+
+	    }
+
+
+	 public Vector<Message> getMessage() {
+
+	        return messages;
+
+	    }
+
+	    
+
+	 public double getSalary() {
+
+	  return salary;
+
+	 }
+
+
+	 public void setSalary(double salary) {
+
+	  this.salary = salary;
+
+	 }
+
+	 
+
+	 public int hashCode() {
+
+	  final int prime = 31;
+
+	  int result = super.hashCode();
+
+	  result = prime * result + Objects.hash(salary);
+
+	  return result;
+
+	 }
+
+
+	 public boolean equals(Object obj) {
+
+	  if (this == obj)
+
+	   return true;
+
+	  if (!super.equals(obj))
+
+	   return false;
+
+	  if (getClass() != obj.getClass())
+
+	   return false;
+
+	  Employee other = (Employee) obj;
+
+	  return Double.doubleToLongBits(salary) == Double.doubleToLongBits(other.salary);
+
+	 }
+
+
+	 
+
+	 public String toString() {
+
+	  return super.toString() + ", salary= " + salary ;
+
+	 }
+
+	 
+
 	}
-
-	public Message getMessage() {
-		return null;
-	}
-}
