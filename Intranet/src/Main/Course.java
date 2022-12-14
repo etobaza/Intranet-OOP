0159bd0c61d74ec64302c7b2297697fa923f4858
kalma <kalma@LAPTOP-NDHO64MP>
2022-12-14 20:41:58 +0600

    begin

--- Intranet/src/Main/Course.java
+++ Intranet/src/Main/Course.java
@@ -1,62 +1,47 @@
 package Main;
+
+import java.util.HashMap;
+import java.util.Vector;
+
 /**
  */
 public class Course {
-    /**
-     */
-    private string name;
-
-    /**
-     */
+    private String name;
     private int credits;
-
-    /**
-     */
-    private string description;
-
-    /**
-     */
-    private string courseId;
-
-    /**
-     */
+    private String description;
+    private String courseId;
     private Vector<Student> student;
-
-    /**
-     */
     private HashMap<Student, Mark> marks;
-
-    /**
-     */
-    private string prerequisite;
-
-    /**
-     */
+    private String prerequisite;
     private boolean isElective;
-
-    /**
-     */
     private Semester semester;
-
-    /**
-     */
     private Faculty faculty;
-
-    /**
-     */
-    public void addStudent() {
+    
+    public Course(String name, int credits, String description, String courseId, String prerequisite, Semester semester, Faculty faculty ) {
+    	this.name=name;
+    	this.credits=credits;
+    	this.description=description;
+    	this.courseId=courseId;
+    	this.prerequisite=prerequisite;
+    	this.semester=semester;
+    	this.faculty=faculty;
+    	student= new Vector<Student>();
     }
-
-    /**
-     * @return 
-     */
-    public boolean removeStudent() {
-        return false;
+    
+    public boolean addStudent() {
+    	if(!student.contains(student)) {
+    		student.addAll(student);
+			return true;
+		}
+		return false;
     }
-
-    /**
-     */
+    
+    public void removeStudent() {
+        student.remove(student);
+    }
+    
     public void dropCourse() {
+    	
     }
 
     /**
