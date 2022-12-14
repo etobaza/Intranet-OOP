1c488ca91583a4648737c09289503c4975a03a61
Khan Martin <rblxxmartin@gmail.com>
2022-12-14 20:14:06 +0600

    studinfo

--- Intranet/src/Main/Student.java
+++ Intranet/src/Main/Student.java
@@ -31,7 +31,11 @@ public class Student extends User implements ViewTranscript {
 		this.academicDegree = academicDegree;
 	}
 
-	public void viewStudentInfo() {
+	public String viewStudentInfo() {
+		return String.format(
+				"First name: %s\nLast name: %s\nID: %s\nUsername: %s\nSex: %s\nAge: %d\nEmail: %s\nFaculty: %s\nEnrollment date: %s\nYear: %d\nSemester: %s\nAcademic degree: %s",
+				this.getFirstName(), this.getLastName(), this.getId(), this.getUsername(), this.getSex(), this.getAge(),
+				this.getEmail(), this.faculty, this.enrollmentDate, this.year, this.semester, this.academicDegree);
 	}
 
 	public void viewAttendance() {
@@ -41,7 +45,7 @@ public class Student extends User implements ViewTranscript {
 	}
 
 	public void viewOrganization() {
-		
+
 	}
 
 	public boolean joinOrganization(OrganizationName organization) {
