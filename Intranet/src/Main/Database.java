76b04463c9657340ce8a41769afc2f025f856d5b
Alikhan <sekfree410@gmail.com>
2022-12-21 16:09:14 +0600

    db

--- Intranet/src/Main/Database.java
+++ Intranet/src/Main/Database.java
@@ -17,18 +17,204 @@ public final class Database {
 	public static Vector<Message> messages = new Vector<Message>();
 	public static Vector<Journal> journals = new Vector<Journal>();
 	public static Vector<Mark> marks = new Vector<Mark>();
-	public static Vector<News> news = new Vector<News>();
+	public static Vector<News> newss = new Vector<News>();
 	public static Vector<Organization> organizations = new Vector<Organization>();
 	public static Vector<Transcript> transcripts = new Vector<Transcript>();
 
 	public static boolean updateStudent(Student student) {
-		if (students.contains(student)) {
-			students.remove(student);
-			students.addElement(student);
-			return true;
-		} else
-			students.addElement(student);
-		return false;
+        if (student == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = students.indexOf(student);
+        if (index >= 0) {
+            students.set(index, student);
+            return true;
+        } else {
+            students.add(student);
+            return false;
+        }
+    }
+	
+	public static boolean updateUser(User user) {
+		if (user == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = users.indexOf(user);
+        if (index >= 0) {
+            users.set(index, user);
+            return true;
+        } else {
+            users.add(user);
+            return false;
+        }
+	}
+	
+	public static boolean updateAdmin(Admin admin) {
+		if (admin == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = admins.indexOf(admin);
+        if (index >= 0) {
+            admins.set(index, admin);
+            return true;
+        } else {
+            admins.add(admin);
+            return false;
+        }
+	}
+	
+	public static boolean updateDeskworker(DeskWorker deskworker) {
+		if (deskworker == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = deskworkers.indexOf(deskworker);
+        if (index >= 0) {
+            deskworkers.set(index, deskworker);
+            return true;
+        } else {
+            deskworkers.add(deskworker);
+            return false;
+        }
+	}
+	
+	public static boolean updateManager(Manager manager) {
+		if (manager == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = managers.indexOf(manager);
+        if (index >= 0) {
+            managers.set(index, manager);
+            return true;
+        } else {
+            managers.add(manager);
+            return false;
+        }
+	}
+	
+	public static boolean updateTeacher(Teacher teacher) {
+		if (teacher == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = teachers.indexOf(teacher);
+        if (index >= 0) {
+            teachers.set(index, teacher);
+            return true;
+        } else {
+            teachers.add(teacher);
+            return false;
+        }
+	}
+	
+	public static boolean updateCourse(Course course) {
+		if (course == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = courses.indexOf(course);
+        if (index >= 0) {
+            courses.set(index, course);
+            return true;
+        } else {
+            courses.add(course);
+            return false;
+        }
+	}
+	
+	public static boolean updateMessage(Message message) {
+		if (message == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = messages.indexOf(message);
+        if (index >= 0) {
+            messages.set(index, message);
+            return true;
+        } else {
+            messages.add(message);
+            return false;
+        }
+	}
+	
+	public static boolean updateJournal(Journal journal) {
+		if (journal == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = journals.indexOf(journal);
+        if (index >= 0) {
+            journals.set(index, journal);
+            return true;
+        } else {
+            journals.add(journal);
+            return false;
+        }
+	}
+	
+	public static boolean updateMark(Mark mark) {
+		if (mark == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = marks.indexOf(mark);
+        if (index >= 0) {
+            marks.set(index, mark);
+            return true;
+        } else {
+            marks.add(mark);
+            return false;
+        }
+	}
+	
+	public static boolean updateNews(News news) {
+		if (news == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = newss.indexOf(news);
+        if (index >= 0) {
+            newss.set(index, news);
+            return true;
+        } else {
+            newss.add(news);
+            return false;
+        }
+	}
+	
+	public static boolean updateOrganization(Organization organization) {
+		if (organization == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = organizations.indexOf(organization);
+        if (index >= 0) {
+            organizations.set(index, organization);
+            return true;
+        } else {
+            organizations.add(organization);
+            return false;
+        }
+	}
+
+	
+	public static boolean updateTranscript(Transcript transcript) {
+		if (transcript == null) {
+            System.out.println("Error: cannot be null");
+            return false;
+        }
+        int index = transcripts.indexOf(transcript);
+        if (index >= 0) {
+            transcripts.set(index, transcript);
+            return true;
+        } else {
+            transcripts.add(transcript);
+            return false;
+        }
 	}
 
 	private Database(String path) {
