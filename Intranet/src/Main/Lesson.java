eb4c1fd872d5c89da7462f774aff8a3c26df5b7e
kalma <kalma@LAPTOP-NDHO64MP>
2022-12-22 04:37:15 +0600

    can u see comments?

--- Intranet/src/Main/Lesson.java
+++ Intranet/src/Main/Lesson.java
@@ -24,71 +24,126 @@ public class Lesson implements Serializable{
 		this.setRoom(room);
 		this.teacher = teacher;
 	}
-
+	/**
+	 * 
+	 * Implements getter of Teacher for Lesson class
+	 */
 	public Teacher getTeacher() {
 		return teacher;
 	}
-
+	/**
+	 * Attendance or not
+	 * 
+	 */
 	public boolean isAttendance() {
 		return attendance;
 	}
-
+	/**
+	 * 
+	 * Implements setter of Attendance for Lesson class
+	 */
 	public void setAttendance(boolean attendance) {
 		this.attendance = attendance;
 	}
 
+	/**
+	 * 
+	 * Implements getter of Course for Lesson class
+	 */
+	
 	public Course getCourse() {
 		return course;
 	}
-
+	/**
+	 * 
+	 * Implements getter of room for Lesson class
+	 */
 	public int getRoom() {
 		return room;
 	}
-
+	/**
+	 * 
+	 * Implements setter of room for Lesson class
+	 */
 	public void setRoom(int room) {
 		this.room = room;
 	}
-
+	/**
+	 * 
+	 * Implements getter of Lesson type for Lesson class
+	 */
 	public LessonType getLessonType() {
 		return lessonType;
 	}
-
+	/**
+	 * 
+	 * Implements setter of Lesson Type for Lesson class
+	 */
 	public void setLessonType(LessonType lessonType) {
 		this.lessonType = lessonType;
 	}
-
+	/**
+	 * 
+	 * Implements getter of Date for Lesson class
+	 */
 	public Date getDate() {
 		return date;
 	}
-
+	/**
+	 * 
+	 * Implements setter of Date for Lesson class
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	
+	/**
+	 * 
+	 * Implements getter of Format for Lesson class
+	 */
 
 	public Format getFormat() {
 		return format;
 	}
-
+	/**
+	 * 
+	 * Implements setter of Format for Lesson class
+	 */
 	public void setFormat(Format format) {
 		this.format = format;
 	}
-
+	/**
+	 * 
+	 * Implements getter of Day for Lesson class
+	 */
 	public Day getDay() {
 		return day;
 	}
-
+	/**
+	 * 
+	 * Implements setter of Day for Lesson class
+	 */
 	public void setDay(Day day) {
 		this.day = day;
 	}
+	/**
+	* Returns the primitive value that we gave to the class of Lesson class
+	*/
 
 	public String toString() {
 		return "Lesson [course=" + course + ", day=" + day + ", format=" + format + ", lessonType=" + lessonType
 				+ ", date=" + date + ", room=" + room + ", attendance=" + attendance + "]";
 	}
+	/**
+	* hashCode() implementation by including  salary of the Lesson class
+	*/
 
 	public int hashCode() {
 		return Objects.hash(attendance, course, date, day, format, lessonType, room);
 	}
+	/**
+	*Implementing equals() for Lesson class
+	*/
 
 	public boolean equals(Object obj) {
 		if (this == obj)
